eaf15bb97e1bb3e9b287370db64aad066680a320
Baraasher <Baraa.programmer@gmail.com>
2023-03-12 16:25:17 +0200

    Edit Main Layout

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,9 @@
 let saveButton = document.getElementById("save-note");
-let mainTextarea = document.getElementById("main-note-textarea");
+let noteInput = document.getElementById("noteInput");
 let notes = document.getElementById("the-notes");
 let deleteButton = document.getElementById("delete-note");
+let slider = document.getElementById("slider");
+let utc = new Date().toJSON().slice(0, 10).replace(/-/g, "/");
 
 //preload
 
@@ -10,21 +12,7 @@ window.addEventListener("load", function () {
   loader.style.display = "none";
 });
 
-let slider = document.getElementById("slider");
-let utc = new Date().toJSON().slice(0, 10).replace(/-/g, "/");
 
-saveButton.addEventListener("click", function () {
-  if (
-    mainTextarea.value === "" ||
-    mainTextarea.value === undefined ||
-    mainTextarea.value === null ||
-    mainTextarea.childNodes.length === 0
-  ) {
-    alert("Add Some Text Please !");
-  } else {
-    createNote();
-  }
-});
 
 //dark mode
 slider.addEventListener("click", function () {
@@ -32,27 +20,54 @@ slider.addEventListener("click", function () {
   document.querySelector("body").classList.toggle("darkmode");
 });
 
+
+
+saveButton.onclick = function () {
+
+
+if(noteInput.value !== "")
+{
+  createNote()
+}
+
+
+else{
+  alert("Please enter a note");
+}
+
+}
+
+
+
+
 function createNote() {
+
+
   let date = document.createElement("span");
   let complete = document.createElement("div");
-  complete.className = "complete-note";
-  let content = document.createElement("div");
-  content.id = "complete-note-content";
+  let content = document.createElement("textarea");
   let deleteNote = document.createElement("button");
+
+
+
+  complete.className = "complete-note";
+  complete.id="complete-note"
+  content.id = "complete-note-content";
   deleteNote.id = "delete-note";
   deleteNote.innerHTML = "<i></i>";
   deleteNote.firstChild.className = "fa-regular fa-trash-can";
   notes.appendChild(complete);
   complete.appendChild(content);
-  content.innerText = mainTextarea.value;
+  content.value = noteInput.value;
   complete.appendChild(date);
   complete.appendChild(deleteNote);
   date.id = "date";
   date.innerHTML = utc;
   document.getElementsByClassName("complete-note").id = "completeNote";
-  mainTextarea.value = "";
+  noteInput.value = "";
+
+
+
 }
 
-document.getElementById("delete-note").addEventListener("click", function () {
-  console.log("delete");
-});
+
